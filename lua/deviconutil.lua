824a78a4f1741489a7c07d564c414ee32ea15d0a
Daniel Horner <daniel_horner@pobox.com>
2024-11-15 13:47:40 -0800

    Enable highlight colors for webdevicons

--- lua/deviconutil.lua
+++ lua/deviconutil.lua
@@ -7,7 +7,8 @@ local fn = vim.fn
 function M.get_icon(bufname)
   local loaded, webdev_icons = pcall(require, "nvim-web-devicons")
   if loaded then
-    return webdev_icons.get_icon(fn.fnamemodify(bufname, ":r"), fn.fnamemodify(bufname, ":e"), { default = true })
+    icon, hlgroup = webdev_icons.get_icon(fn.fnamemodify(bufname, ":r"), fn.fnamemodify(bufname, ":e"), { default = true })
+    return '%#' .. hlgroup .. '#' .. icon
   end
   local devicons_loaded = fn.exists("*WebDevIconsGetFileTypeSymbol") > 0
   return devicons_loaded and fn.WebDevIconsGetFileTypeSymbol(bufname) or ""
